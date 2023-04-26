e6a03a252aa8ed5e278138c64a3d52d51bca945c
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-04-26 19:55:52 -0400

    Added useState, useRef hooks and App.css import for App component

--- chatbox/src/App.js
+++ chatbox/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const messageEnd = useRef(null);
+  const [ans, setAns] = useState([]);
+  const [addAsk, setAsk] = useState("");
 
-export default App;
+  const [typing, setTyping] = useState(false);

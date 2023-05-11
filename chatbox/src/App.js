05a2aba6a2b07c1731b26c1abe40201130db1c36
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-05-11 15:16:27 -0400

    Authpage frontend  created

--- chatbox/src/App.js
+++ chatbox/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useState, useRef,  } from "react";
 import IntroPage from "./IntroPage";
+import AuthPage from "./Authpage";
+
 
 
 
@@ -12,6 +14,8 @@ function App() {
   const [addAsk, setAsk] = useState("");
   const [typing, setTyping] = useState(false);
   const [showIntro, setShowIntro] = useState(true);
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
   
   const handleAdd = async () => {
     setTimeout(() => {
@@ -69,87 +73,124 @@ function App() {
   };
 
  
-
   return (
     <>
       {showIntro ? (
         <IntroPage onDismiss={handleIntroDismiss} />
       ) : (
-        <div className="apiApp">
-          <ul className='chatContainer'>
-            <div className="chatSec">
-              {typing && (
-                <div className="chat">
-                  <div className="chatCont">
-                    <div className="assistant typing">
-                      <div className="dot"></div>
-                      <div className="dot"></div>
-                      <div className="dot"></div>
-                    </div>
-                  </div>
-                </div>
-              )}
-              {ans.length > 0 ? (
-                ans.map((ans, index) => {
-                  return (
-                    <div className="chat" key={index}>
-                      <div
-                        className={
-                          ans.role === "Assistant" ? "chatCont" : "chatContUser"
-                        }
-                      >           
-                      <p
-                          className={
-                            ans.role === "User"
-                              ? "user chatStyle"
-                              : "assistant chatStyle"
-                          }
-                        >
-                          {ans.responsed}
-                        </p>
+        <>
+          {isAuthenticated ? (
+            <div className="apiApp">
+              <ul className="chatContainer">
+                <div className="chatSec">
+                  {typing && (
+                    <div className="chat">
+                      <div className="chatCont">
+                        <div className="assistant typing">
+                          <div className="dot"></div>
+                          <div className="dot"></div>
+                          <div className="dot"></div>
+                        </div>
                       </div>
                     </div>
-                  );
-                })
-              ) : (
-                <p style={{ margin: "0 auto" }}>
-                <strong>"Ask me anything!"</strong><br />
-                <strong>"What would you like to know about?"</strong><br />
-                <strong>"I can help with a variety of topics - try asking about science, history, or literature."</strong><br />
-                <strong>"Do you need help with a specific problem or situation?"</strong><br />
-                <strong>"Tell me about your day - I'm here to chat!"</strong><br />
-                <strong>"I can also recommend books, movies, or TV shows - want a suggestion?"</strong><br />
-                <strong>"Curious about a particular topic? Ask me and I'll do my best to answer!"</strong><br />
-                <strong>"Need some advice or just want to vent? I'm here to listen."</strong><br />
-                <strong>"Let's have a conversation! What's on your mind?"</strong><br />
-                <strong>"I can also tell jokes or fun facts - want to hear one?"</strong>
-              </p>
-              
-              
-              )}
-              <div ref={messageEnd} />
+                  )}
+                  {ans.length > 0 ? (
+                    ans.map((ans, index) => {
+                      return (
+                        <div className="chat" key={index}>
+                          <div
+                            className={
+                              ans.role === "Assistant"
+                                ? "chatCont"
+                                : "chatContUser"
+                            }
+                          >
+                            <span
+                              className={
+                                ans.role === "User"
+                                  ? "user chatStyle"
+                                  : "assistant chatStyle"
+                              }
+                            >
+                              {ans.responsed}
+                            </span>
+                          </div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <p style={{ margin: "0 auto" }}>
+                      <strong>"Ask me anything!"</strong>
+                      <br />
+                      <strong>"What would you like to know about?"</strong>
+                      <br />
+                      <strong>
+                        "I can help with a variety of topics - try asking
+                        about science, history, or literature."
+                      </strong>
+                      <br />
+                      <strong>
+                        "Do you need help with a specific problem or
+                        situation?"
+                      </strong>
+                      <br />
+                      <strong>
+                        "Tell me about your day - I'm here to chat!"
+                      </strong>
+                      <br />
+                      <strong>
+                        "I can also recommend books, movies, or TV shows -
+                        want a suggestion?"
+                      </strong>
+                      <br />
+                      <strong>
+                        "Curious about a particular topic? Ask me and I'll do
+                        my best to answer!"
+                      </strong>
+                      <br />
+                      <strong>
+                        "Need some advice or just want to vent? I'm here to
+                        listen."
+                      </strong>
+                      <br />
+                      <strong>
+                        "Let's have a conversation! What's on your mind?"
+                      </strong>
+                      <br />
+                      <strong>
+                        "I can also tell jokes or fun facts - want to hear
+                        one?"
+                      </strong>
+                    </p>
+                  )}
+                  <div ref={messageEnd} />
+                </div>
+              </ul>
+              <div className="addPost">
+                <input
+                  type="text"
+                  value={addAsk}
+                  placeholder="Lets chat :)"
+                  onChange={(e) => setAsk(e.target.value)}
+                  onKeyDown={saveEnter}
+                />
+                <button
+                  className="sendBtn"
+                  onClick={handleAdd}
+                  disabled={!addAsk}
+                >
+                  Send
+                </button>
+              </div>
             </div>
-          </ul>
-          <div className="addPost">
-            <input
-              type="text"
-              value={addAsk}
-              placeholder="Lets chat :)"
-              onChange={(e) => setAsk(e.target.value)}
-              onKeyDown={saveEnter}
-            />
-            <button
-              className='sendBtn'
-              onClick={handleAdd}
-              disabled={!addAsk}
-            >
-              Send
-            </button>
-          </div>
-        </div>
+          ) : (
+            <AuthPage onAuthentication={setAuthenticated} />
+          )}
+        </>
       )}
     </>
   );
+  
   
 }
 
---
+++ chatbox/src/Authpage.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const AuthPage = ({ onAuthentication }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegisterMode, setRegisterMode] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    //  authentication logic based on the form mode (login or registration)
+    if (isRegisterMode) {
+      // Registration logic
+      
+
+      console.log('Registration:', email, password);
+      // Call the authentication callback to update the authentication status in the App component
+      onAuthentication(true);
+    } else {
+      // Login logic
+      //
+      // 
+      console.log('Login:', email, password);
+      // Call the authentication callback to update the authentication status in the App component
+      onAuthentication(true);
+    }
+
+    // Reset form fields
+    setEmail('');
+    setPassword('');
+  };
+
+  const toggleMode = () => {
+    setRegisterMode(!isRegisterMode);
+  };
+
+  return (
+    <div className="auth-container">
+      <h2>{isRegisterMode ? 'Register' : 'Login'}</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={handleEmailChange} required />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+        <button type="submit">{isRegisterMode ? 'Register' : 'Login'}</button>
+      </form>
+      <p>
+        {isRegisterMode ? 'Already have an account?' : "Don't have an account?"}{' '}
+        <button onClick={toggleMode}>{isRegisterMode ? 'Login' : 'Register'}</button>
+      </p>
+    </div>
+  );
+};
+
+export default AuthPage;

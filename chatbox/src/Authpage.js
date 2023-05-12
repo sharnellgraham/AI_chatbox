46635ffabd5d301eb215f7901869f88fe8c28ce8
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-05-12 15:45:09 -0400

    Auth / intro page edit

--- chatbox/src/Authpage.js
+++ chatbox/src/Authpage.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './Authpage.css';
 
-
 const AuthPage = ({ onAuthentication }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [isRegisterMode, setRegisterMode] = useState(false);
+  const [error, setError] = useState('');
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,33 +16,42 @@ const AuthPage = ({ onAuthentication }) => {
     setPassword(e.target.value);
   };
 
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    //  authentication logic based on the form mode (login or registration)
     if (isRegisterMode) {
       // Registration logic
-      
+      if (password !== confirmPassword) {
+        setError('Passwords do not match');
+        return;
+      }
+
+      // Additional validation logic for email and password fields can be added here
 
       console.log('Registration:', email, password);
-      // Call the authentication callback to update the authentication status in the App component
       onAuthentication(true);
     } else {
       // Login logic
-      //
-      // 
+      // Additional validation logic for email and password fields can be added here
+
       console.log('Login:', email, password);
-      // Call the authentication callback to update the authentication status in the App component
       onAuthentication(true);
     }
 
-    // Reset form fields
+    // Reset form fields and error state
     setEmail('');
     setPassword('');
+    setConfirmPassword('');
+    setError('');
   };
 
   const toggleMode = () => {
     setRegisterMode(!isRegisterMode);
+    setError('');
   };
 
   return (
@@ -52,6 +62,13 @@ const AuthPage = ({ onAuthentication }) => {
         <input type="email" value={email} onChange={handleEmailChange} required />
         <label>Password:</label>
         <input type="password" value={password} onChange={handlePasswordChange} required />
+        {isRegisterMode && (
+          <>
+            <label>Confirm Password:</label>
+            <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
+          </>
+        )}
+        {error && <p className="error-message">{error}</p>}
         <button type="submit">{isRegisterMode ? 'Register' : 'Login'}</button>
       </form>
       <p>
--- chatbox/src/IntroPage.js
+++ chatbox/src/IntroPage.js
@@ -2,15 +2,32 @@ import React from "react";
 import "./IntroPage.css";
 
 function IntroPage({ onDismiss }) {
-    return (
-      <div className="introPage">
-        <img src="https://i.ibb.co/tx5Ctbb/1698535.png" alt="Chatbot" class="chatbot-image" />
+  const handleImageClick = () => {
+    onDismiss();
+  };
 
-        <h1><span>W</span><span>E</span><span>L</span><span>C</span><span>O</span><span>M</span><span>E</span></h1>
-        <p> When you're ready, simply hit the "Close" button below to begin</p>
-        <button onClick={onDismiss}>Close</button>
-      </div>
-    );
+  return (
+    <div className="introPage">
+      <img
+        src="https://i.ibb.co/tx5Ctbb/1698535.png"
+        alt="Chatbot"
+        className="chatbot-image"
+        onClick={handleImageClick}
+      />
+
+      <h1>
+        <span>W</span>
+        <span>E</span>
+        <span>L</span>
+        <span>C</span>
+        <span>O</span>
+        <span>M</span>
+        <span>E</span>
+      </h1>
+      <p>When you're ready, click robot   to begin</p>
+     
+    </div>
+  );
 }
 
 export default IntroPage;
